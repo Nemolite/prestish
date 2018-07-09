4d5bacb996e3bd6c9c100cdf199dff045af0af8d
Nemolite <g25092011@mail.ru>
2018-07-09 15:02:34 +0300

    js-code

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,21 @@ $(window).load(function() {
     controlNav: "thumbnails"
   });
 });
+
+// плавный скорлинг
+
+$(document).ready(function(){
+	$("#id-foot-middle,#id-top-menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+});
